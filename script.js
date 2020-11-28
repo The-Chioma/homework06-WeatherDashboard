b9151f71339ec1f67749854c1ed75b167f42295e
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2020-11-28 00:54:52 +0000

    created my function that will create html elements and display weather information on webpage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- //assigning my variables
+ //assigning my variables brought in from html file
  var citiesSearched = $("#cities-searched");
  var listOfCities = $(".history");
  var currentWeather = $("#today-weather");
@@ -6,59 +6,67 @@
  var searchButton = $("#search-button");
 
  
-
- function getForecast(userInput){
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=886705b4c1182eb1c69f28eb8c520e20&units=metric"
+//this function will get the information for the current weather and display it on the page
+ function getCurrentWeather(userInput){
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=a8a6cbd561df9120a6feee123727868c&units=metric"
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response)
 
-        {   var cityName = response.name;
-            var temp = response.main.temp + " ºC";
-            var humidity = response.main.humidity + "%";
-            var windSpeed = response.wind.speed + " MPH";
-            var longitude = response.coord.lon;
+        {   var longitude = response.coord.lon;
             var latitude = response.coord.lat;
 
-            function getUVindex() {
-                var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude+ "&appid=886705b4c1182eb1c69f28eb8c520e20&units=metric"
+            //creating html elements that will appear on the page once search-button is clicked
+            var cityName = $("<h3>").addClass("card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
+            var card = $("<div>").addClass("card");
+            var windSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed + " MPH");
+            var humidity = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + "%");
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp + " °C");
+            var cardBody = $("<div>").addClass("card-body");
+            var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+
+            //obtaining the value of the uv index
+            function getUVindex(latitude, longitude) {
+                var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=886705b4c1182eb1c69f28eb8c520e20&"
                 $.ajax({
                     url: uvURL,
                     method: "GET"
                 }).then(function(response){
-                    var uvIndex = getUVindex();
-                    //console.log(uvIndex);
+                    var uvBtn = $("<span>").addClass("btn btn-sm").text(response.value);
+                    var uv = $("<p>").text("UV Index: ");
+                    
+                    // change color of the UV index button depending on uv value
+                    if (response.value < 3) {
+                      uvBtn.addClass("btn-success");
+                    }
+                    else if (response.value < 7) {
+                      uvBtn.addClass("btn-warning");
+                    }
+                    else {
+                      uvBtn.addClass("btn-danger");
+                    }
+
+                    //appending all elements to eachother and then to the page
+                    uv.append(uvBtn);
+                    cityName.append(img);
+                    cardBody.append(cityName, temp, humidity, windSpeed, uv);
+                    card.append(cardBody);
+                    currentWeather.append(card);
+                    
                 })
-            }
-
-            
-
-            //http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
-           
-            console.log(cityName);
-            console.log(temp);
-            console.log(humidity);
-            console.log(windSpeed);
-            console.log(longitude);
-            console.log(latitude);
-           
-            var newDiv = $("<div>");
-            newDiv.text(cityName)
-            citiesSearched.append(newDiv);
+            } getUVindex(latitude,longitude);
+              
             
         })
     }
 
     searchButton.on("click", function(){
         var userInput = $("#search-input").val().trim();
-        alert ("The user typed " + userInput);
-        return getForecast(userInput);
+        currentWeather.empty();
+        return getCurrentWeather(userInput);
 
        
         
     })
     
-    
- 
-  //&appid=a8a6cbd561df9120a6feee123727868c
